94fdd520b23b50500b3fb3b6c345ed6bd459c978
Dr Heinz M. Kabutz <heinz@supersolid-5.local>
2010-08-10 19:42:05 +0300

    added package-info.java file to describe main package

---
+++ src/main/java/org/jpatterns/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * jpatterns.org was conceived during one of the Java Specialists Club design
+ * patterns sessions as a way to clearly label classes as belonging to some
+ * pattern structure using Java annotations.  The project is led by Michael
+ * Hunger and Heinz Kabutz.
+ */
+package org.jpatterns.gof;
