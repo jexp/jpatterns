68dacce62bcdfa8118de83a9f34cb3484b54b6bd
mtedone <mtedone@192.168.1.64>
2010-08-21 18:55:33 +0100

    Added MVC implementation and tests

---
+++ src/main/java/org/jpatterns/ee/ModelViewController.java
@@ -0,0 +1,62 @@
+package org.jpatterns.ee;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.jpatterns.core.DesignPattern;
+import org.jpatterns.core.Type;
+
+/**
+ * <b> Intent: The Model View Controller (MVC) pattern divides an application in
+ * three components: The View, the Controller and the Model.</b>
+ * <ul>
+ * <li><b>The View</b> presents data to clients</li>
+ * <li><b>The Controller</b> handles client's requests, uses the Model to get
+ * things done and dispatches to the appropriate view a response to the request
+ * (this response might contain objects from the Model)</li>
+ * <li><b>The Model</b> wraps the logic and the domain model of the application,
+ * offering both services and objects to the view</li>.
+ * </ul>
+ * <p/>
+ * <img src="http://www.jpatterns.org/uml/ee/MVCStructure.gif"/>
+ * <p/>
+ * 
+ * @author Marco Tedone
+ * @since 2010-08-21
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target( { ElementType.TYPE })
+@Documented
+@DesignPattern(type = Type.ENTERPRISE, related = { BusinessDelegatePattern.class })
+public @interface ModelViewController {
+
+	String comment() default "";
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.TYPE)
+	@Documented
+	@DesignPattern(type = Type.ENTERPRISE)
+	public @interface View {
+		String comment() default "";
+	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.TYPE)
+	@Documented
+	@DesignPattern(type = Type.ENTERPRISE)
+	public @interface Controller {
+		String comment() default "";
+	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.TYPE)
+	@Documented
+	@DesignPattern(type = Type.ENTERPRISE)
+	public @interface Model {
+		String comment() default "";
+	}
+
+}
