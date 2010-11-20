ab46639a6b7f890a8fb147afe38127bee12b3007
Alex Gout <alex@iMac.local>
2010-11-20 19:10:42 +0100

    fixed factorymethod test

--- src/test/java/org/jpatterns/gof/FactoryMethodTest.java
+++ src/test/java/org/jpatterns/gof/FactoryMethodTest.java
@@ -2,7 +2,7 @@ package org.jpatterns.gof;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.util.*;
 
 /**
  * @author Alex Gout
@@ -10,62 +10,92 @@ import static org.junit.Assert.assertEquals;
  */
 public class FactoryMethodTest {
 
-  @FactoryMethodPattern.Product(participants = VehicleFactory.class)
-  private static interface Vehicle {
-    public String echo();
+  @FactoryMethodPattern.Product(participants = NumberPrinter.class)
+  private static interface NumberGenerator {
+    public int next();
   }
 
-  @FactoryMethodPattern.Creator(participants = Vehicle.class)
-  private static interface VehicleFactory {
-    public Vehicle createVehicle();
+  @FactoryMethodPattern.Creator(participants = NumberGenerator.class)
+  private static abstract class NumberPrinter {
+
+    public void printNumbers(int _upto) {
+      Collection<Integer> result = new ArrayList<Integer>();
+      NumberGenerator numberGenerator = createNumberGenerator();
+      int curNum = numberGenerator.next();
+      while (curNum < _upto) {
+        result.add(Integer.valueOf(curNum));
+        curNum = numberGenerator.next();
+      }
+      System.out.println(result);
+    }
+
+    protected abstract NumberGenerator createNumberGenerator();
   }
 
   @FactoryMethodPattern.ConcreteProduct
-  private static class Car implements Vehicle {
+  private static class PrimeGenerator implements NumberGenerator {
+    int number = 0;
 
-    public String echo() {
-      return "Car!";
+
+    public int next() {
+      while (!isPrime(number)) {
+        number++;
+      }
+      return number++;
+    }
+
+    boolean isPrime(int n) {
+      if (n % 2 == 0) return false;
+      for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0)
+          return false;
+      }
+      return true;
     }
   }
 
   @FactoryMethodPattern.ConcreteCreator
-  private static class CarFactory implements VehicleFactory {
+  private static class PrimePrinter extends NumberPrinter {
 
-    public Vehicle createVehicle() {
-      return new Car();
+    @Override
+    protected NumberGenerator createNumberGenerator() {
+      return new PrimeGenerator();
     }
   }
 
   @FactoryMethodPattern.ConcreteProduct
-  private static class Bike implements Vehicle {
+  private static class FibonacciGenerator implements NumberGenerator {
+    private int fibPos = 0;
 
-    public String echo() {
-      return "Bike!";
+    public int next() {
+      return fibonacciNumber(fibPos++);
+    }
+
+    private int fibonacciNumber(int _pos) {
+      if (_pos <= 1) return _pos;
+      return fibonacciNumber(_pos - 1) + fibonacciNumber(_pos - 2);
     }
   }
 
   @FactoryMethodPattern.ConcreteCreator
-  private static class BikeFactory implements VehicleFactory {
+  private static class FibonacciPrinter extends NumberPrinter {
 
-    public Vehicle createVehicle() {
-      return new Bike();
+    @Override
+    protected NumberGenerator createNumberGenerator() {
+      return new FibonacciGenerator();
     }
   }
 
   @Test
-  public void testCarFactory() {
-    VehicleFactory factory = new CarFactory();
-    Vehicle vehicle = factory.createVehicle();
-
-    assertEquals("Car!", vehicle.echo());
+  public void testFibonacciPrinter() {
+    NumberPrinter numPrinter = new FibonacciPrinter();
+    numPrinter.printNumbers(1000);
   }
 
   @Test
-  public void testBikeFactory() {
-    VehicleFactory factory = new BikeFactory();
-    Vehicle vehicle = factory.createVehicle();
-
-    assertEquals("Bike!", vehicle.echo());
+  public void testPrimePrinter() {
+    NumberPrinter numPrinter = new PrimePrinter();
+    numPrinter.printNumbers(1000);
   }
 
 
